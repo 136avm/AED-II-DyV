9ad8b9c52cb42d17fd2dc51df2bc038b086f8162
136avm <a.vergaramoya@um.es>
2024-03-20 19:50:12 +0100

    Arreglo de caso promedio

--- generador.h
+++ generador.h
@@ -7,6 +7,7 @@ using namespace std;
 
 string generarCadena(int n) {
     string cadena = "";
+    srand(time(0));
     for(int i=0; i<n; i++) {
         cadena += (char)('a' + (rand() % 26));
     }
